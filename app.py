1066d6404e896bd33502fb691214e2713d269c46
mttaggart <michael.t.taggart@gmail.com>
2019-04-09 19:59:16 -0700

    error handling

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, abort, request, render_template
 from pw import generate_password, password_list
 
 app = Flask(__name__)
@@ -14,18 +14,28 @@ def index():
     pw = generate_password()
     return render_template("index.html", pw=pw)
 
-@app.route("/api")
-def api_options():
-    return "/api/pw\n/api/pwlist"
+@app.route("/api", methods=["GET","POST"])
+def api():
+    return render_template("api.html", n=10, sep="_", digit_min=10, digit_max=99)
 
 @app.route("/api/pw")
 def get_password():
     return generate_password()
 
+@app.errorhandler(400)
+def bad_request(error):
+    return "Your API request did not match the specified parameters."
+
 @app.route("/api/pwlist")
 def get_passwords():
     n = int(request.args.get("n", 10))
+    if n > 100:
+        abort(400)
+    
     sep = request.args.get("sep", " ")
     digit_min = int(request.args.get("digitMin", 10))
     digit_max = int(request.args.get("digitMax", 99))
-    return "<br/>".join(password_list(n, sep, digit_min, digit_max))
+    return "\n".join(password_list(n, sep, digit_min, digit_max))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
